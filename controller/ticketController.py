5b0ad5de3cfea98e4495a73ccaa39cb08ded2c03
YowKuan <yoyo6213@gmail.com>
2021-11-24 16:04:08 -0500

    develop all tickets & single ticket controller

--- controller/ticketController.py
+++ controller/ticketController.py
@@ -1,13 +1,13 @@
 import sys
 from os.path import dirname, abspath
 
-from view.appView import appView
+from view.appView import AppView
 from model.requestHandler import requestHandler
 
 class TicketController:
     def __init__(self):
-        self.view = appView
-        self.api = requestHandler
+        self.view = AppView()
+        self.api = requestHandler()
         self.input = ''
         self.id = 0
         self.page = 0
@@ -25,24 +25,83 @@ class TicketController:
             elif self.input == '1':
                 response = self.showAllTickets()
                 if not response:
-                    self.view.display_message("couldn't get all tickets")
+                    self.view.display_message("couldn't get all tickets\n", 1)
             elif self.input == '2':
                 response = self.showOneTicket()
                 if not response:
-                    self.view.display_message("couldn't get the ticket")
+                    self.view.display_message("", 1)
             elif self.input == 'q' or self.input == 'Q':
-                self.view.display_message("Thank you for using our ticket checking system, bye.")
+                self.view.display_message("Thank you for using our ticket checking system, bye.", 0)
                 break
             else:
-                self.view.display_message("Invalid input. Please type menu to see all options")
+                self.view.display_message("Invalid input. Please type menu to see all options\n", 1)
         self.input = ''
     
     def showAllTickets(self):
         try:
-            tickets = self.api.getTickets()
-        except:
-            return
+            self.view.fetchTickets("all")  # Fetching display message
+            tickets = self.api.get_tickets()  # Get all tickets
+            assert tickets not in [401, 404, 503, -1]
+            page = self.view.displayTickets(tickets, 1)
+        except AssertionError as e:
+            self.view.errorCode = self.api.errorCode
+            if tickets == 401:  # No tickets on account
+                self.view.displayBadRequest("No tickets on account to display")
+            elif tickets == 404:  # Can't authenticate with API
+                self.view.displayBadRequest("API authentication not permitted or invalid user credentials.")
+            elif tickets == 503:  # API unavailable
+                self.view.displayBadRequest("API unavailable. Please try again later")
+            elif tickets -1:  # Other Bad Requests
+                self.view.displayBadRequest("Unknown Bad Request")
+            self.view.errorCode = None
+            self.api.errorCode = None
+            return None
+        while True:
+            self.input = input()
+            if self.input == 'q':  # Quit app
+                sys.exit(self.view.quit())  # Print quit message and quit
+            elif self.input == "menu":  # Show menu
+                self.view.show_menu()
+                break
+            elif self.input == "d":  # Page down
+                page += 1
+                page = self.view.displayTickets(tickets, page)
+            elif self.input == "u":  # Page up
+                page -= 1
+                page = self.view.displayTickets(tickets, page)
+            else:
+                self.view.displayInputMessage(
+                    "Page command error. 'd' to go down, 'u' to go up, 'menu' for menu and 'q' for quit: ", 1)
+                # Invalid user input for ticket paging
+            self.input = ""
+            self.currPage = page
+        return 0
+    def showOneTicket(self):  # Controller method for displaying one ticket in view
+        self.view.display_message("Enter the ticket ID: ", 0)  # Display ticket ID input message
+        self.input = input() # Get ticket ID
+        ticketID = self.input
+        self.input = ""
+        try:
+            self.view.fetchTickets(ticketID)  # Get ticket
+            ticket = self.api.getTicket(ticketID)
+            assert ticket not in [401, 404, 503, -1]
+            self.view.displayTicket(ticket)  # Display ticket
+            self.currID = int(ticketID)  # Current ticket ID
+            return 1
+        except AssertionError as e:
+            self.view.errorCode = self.api.errorCode
+            if ticket == 401:  # Can't authenticate with API
+                self.view.displayBadRequest("API authentication not permitted or invalid user credentials.")
+            elif ticket == 404:  # Ticket ID not valid
+                self.view.displayBadRequest("The ticket ID you gave is not a valid ID")
+            elif ticket == 503:  # API unavailable
+                self.view.displayBadRequest("API unavailable. Please try again later")
+            elif ticket -1:  # Other Bad Requests
+                self.view.displayBadRequest("Unknown Bad Request")
+            self.view.errorCode = None
+            self.api.errorCode = None
+            return False
                 
-                
-        
+                    
+            
 
